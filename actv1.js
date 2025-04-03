6201fd5d376d26d13841cde87cbd9ca6eeb4fb79
David8633 <davidinstituto282@gmail.com>
2025-04-03 11:57:16 +0200

    corregida actividad 3 de math

--- actv1.js
+++ actv1.js
@@ -3,5 +3,6 @@ Math.round(Math.random() * 100)
 /*Muestra por consola el valor de PI.*/
 Math.PI
 /*Genera un número aleatorio entre 5 y 10 redondeando y muéstralo por consola. Para  este caso lo recomendado es utilizar la fórmula (Math.random()* (max-min)+min).*/
+Marth.random(*(10 - 5 + 1))+5);
 /*Comprueba el funcionamiento del método sign(). */
 Math.sign(Math.random() * -1)
